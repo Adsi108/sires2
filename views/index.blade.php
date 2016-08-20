bfd7934ebc5351c64aa9fe3a8363e5cc34192be2
Adsi108 <arcangel2255@hotmail.com>
2016-08-20 16:09:29 -0500

    Subo los cambios que le hice a la pagina index con su respectiva traduccion por favor denme ideas de que puedo hacer en el diseño

--- views/index.blade.php
+++ views/index.blade.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>SIRES VERSION 1.8 | </title>
+    <title>SIRES VERSION 1.8  </title>
 
     <!-- Bootstrap -->
     <link href="{{ url () }}/resources/others/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
